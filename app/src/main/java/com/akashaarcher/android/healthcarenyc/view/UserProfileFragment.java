e4b49cf8104da618faae5fa6b969dc9e72d51f53
Akasha Archer <Akasha@LLD-IMAC.home>
2018-06-18 07:36:18 -0400

    edit user profile feature

--- app/src/main/java/com/akashaarcher/android/healthcarenyc/view/UserProfileFragment.java
+++ app/src/main/java/com/akashaarcher/android/healthcarenyc/view/UserProfileFragment.java
@@ -1,12 +1,16 @@
 package com.akashaarcher.android.healthcarenyc.view;
 
+import android.media.Image;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.akashaarcher.android.healthcarenyc.R;
 
@@ -18,10 +22,22 @@ import butterknife.Unbinder;
  * Created by akashaarcher on 4/7/18.
  */
 
-public class UserProfileFragment extends Fragment {
+public class UserProfileFragment extends Fragment implements View.OnClickListener {
+
+    @BindView(R.id.profile_user_greeting)
+    TextView profileUserGreetingTv;
+
+    @BindView(R.id.edit_profile_btn)
+    Button editProfileBtn;
+
+    @BindView(R.id.approve_profile_edit)
+    ImageView approveProfileChangesIv;
+
+    @BindView(R.id.cancel_profile_edit)
+    ImageView cancelProfileChangesIv;
 
     @BindView(R.id.profile_name_edit)
-    EditText userNameEt;
+    EditText profileNameEt;
 
     @BindView(R.id.profile_income_edit)
     EditText familyIncomeEt;
@@ -29,12 +45,22 @@ public class UserProfileFragment extends Fragment {
     @BindView(R.id.profile_family_size_edit)
     EditText familySizeEt;
 
+    @BindView(R.id.profile_name_set)
+    TextView profileNameValueTv;
+
+    @BindView(R.id.profile_income_set)
+    TextView familyIncomeValueTv;
+
+    @BindView(R.id.profile_family_size_set)
+    TextView familySizeValueTv;
+
     private Unbinder unbinder;
+
+    private String displayName;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
 
     @Nullable
@@ -43,6 +69,7 @@ public class UserProfileFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_user_profile, container, false);
         unbinder = ButterKnife.bind(this, view);
 
+        setListeners();
         return view;
     }
 
@@ -52,5 +79,63 @@ public class UserProfileFragment extends Fragment {
         unbinder.unbind();
     }
 
+
+    private void setListeners() {
+        editProfileBtn.setOnClickListener(this);
+        approveProfileChangesIv.setOnClickListener(this);
+        cancelProfileChangesIv.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.edit_profile_btn:
+                editProfileBtn.setVisibility(View.INVISIBLE);
+                approveProfileChangesIv.setVisibility(View.VISIBLE);
+                cancelProfileChangesIv.setVisibility(View.VISIBLE);
+
+                profileNameValueTv.setVisibility(View.INVISIBLE);
+                profileNameEt.setVisibility(View.VISIBLE);
+                familyIncomeValueTv.setVisibility(View.INVISIBLE);
+                familyIncomeEt.setVisibility(View.VISIBLE);
+                familySizeValueTv.setVisibility(View.INVISIBLE);
+                familySizeEt.setVisibility(View.VISIBLE);
+                break;
+
+            case R.id.approve_profile_edit:
+                editProfileBtn.setVisibility(View.VISIBLE);
+                approveProfileChangesIv.setVisibility(View.INVISIBLE);
+                cancelProfileChangesIv.setVisibility(View.INVISIBLE);
+
+                displayName = profileNameEt.getText().toString();
+                profileNameEt.setVisibility(View.INVISIBLE);
+                profileNameValueTv.setText(displayName);
+                profileNameValueTv.setVisibility(View.VISIBLE);
+
+                familyIncomeValueTv.setText(null);
+                familyIncomeValueTv.setVisibility(View.VISIBLE);
+                familyIncomeEt.setVisibility(View.INVISIBLE);
+
+                familySizeValueTv.setText(null);
+                familySizeValueTv.setVisibility(View.VISIBLE);
+                familySizeEt.setVisibility(View.INVISIBLE);
+
+                profileUserGreetingTv.setText("Hello " + displayName);
+                break;
+
+            case R.id.cancel_profile_edit:
+                editProfileBtn.setVisibility(View.VISIBLE);
+                approveProfileChangesIv.setVisibility(View.INVISIBLE);
+                cancelProfileChangesIv.setVisibility(View.INVISIBLE);
+
+                profileNameValueTv.setVisibility(View.VISIBLE);
+                profileNameEt.setVisibility(View.INVISIBLE);
+                familyIncomeValueTv.setVisibility(View.VISIBLE);
+                familyIncomeEt.setVisibility(View.INVISIBLE);
+                familySizeValueTv.setVisibility(View.VISIBLE);
+                familySizeEt.setVisibility(View.INVISIBLE);
+                break;
+        }
+    }
 
 }
